12b9ed3b5b3f27132e3fc44420f127d7aac219f7
MlhSlckr <selcukermelih@gmail.com>
2021-09-19 12:00:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-const nextBtn = document.querySelector('.next');
-const prevBtn = document.querySelector('.prev');
-const slides = document.querySelectorAll('.slider');
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+const slides = document.querySelectorAll(".slider");
 
 let counter = 0;
 
 slides.forEach(function (slider, index) {
   slider.style.left = `${index * 100}%`;
-})
+});
 
 function carousel() {
   if (counter > slides.length - 1) {
     counter = 0;
   }
   if (counter < 0) {
-    counter = slides.length - 1
+    counter = slides.length - 1;
   }
   slides.forEach(function (slider) {
-    slider.style.transform = `translateX(-${counter * 100}%)`
-  })
+    slider.style.transform = `translateX(-${counter * 100}%)`;
+  });
 }
 
-nextBtn.addEventListener('click', () => {
+nextBtn.addEventListener("click", () => {
   counter++;
   carousel();
-})
+});
 
-prevBtn.addEventListener('click', () => {
+prevBtn.addEventListener("click", () => {
   counter--;
   carousel();
-})
+});
